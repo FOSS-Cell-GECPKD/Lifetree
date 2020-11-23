4000399c6fbc090cdd6a34be7f85327b76bdde63
Alwin Joseph <alwinjoseph48@gmail.com>
2020-11-23 19:34:24 +0530

    Removed default author name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import './App.css'
-import Graph from "./graph"
+import React from "react";
+import "./App.css";
+import Graph from "./graph";
 
 /* TODO:
     Add class-names to elements so they can be styled
  */
 
 function App() {
-    return (<React.Fragment>
-            <div className="header">
-                <h1>LifeTree</h1>
-            </div>
+  return (
+    <React.Fragment>
+      <div className="header">
+        <h1>LifeTree</h1>
+      </div>
 
-            <div>
-                <Graph/>
-            </div>
-        </React.Fragment>
-    );
+      <>
+        <Graph />
+      </>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/graph.js
+++ src/graph.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Gitgraph} from "@gitgraph/react";
-import {exportComponentAsPNG} from "react-component-export-image";
+import { Gitgraph } from "@gitgraph/react";
+import { exportComponentAsPNG } from "react-component-export-image";
+import {templateExtend, TemplateName} from "@gitgraph/core";
 
 /* TODO:
     Add class-names to elements so they can be styled
@@ -8,114 +9,137 @@ import {exportComponentAsPNG} from "react-component-export-image";
 
 //  This class renders git-graph
 class Graph extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            branches: [],
-            branchName:''
-        };
-        this.componentRef = React.createRef();
-    }
-
-    // Function to add commit from state
-    addCommit = (branch) => {
-
-        branch.commit(this.state[`commitMessage${branch.name}`]);
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      branches: [],
+      branchName: "",
     };
+    this.componentRef = React.createRef();
 
-    //Function to add branch and adds branch name to branches array in state
-    addBranch = () => {
-        if (this.state.branches
-            .map((b) => b.name)
-            .includes(this.state.branchName)
-        )
-                  return;
+  }
 
-        this.setState(() => ({
-            branches: [...this.state.branches,this.state.gitgraph.branch(this.state.branchName)]
-        }));
-    };
+  // Function to add commit from state
+  addCommit = (branch) => {
+    branch.commit(this.state[`commitMessage${branch.name}`]);
+  };
 
-    // Handles input changes
-    handleChange = (name) => (e) => {
-        this.setState({[name]: e.currentTarget.value});
-    };
+  //Function to add branch and adds branch name to branches array in state
+  addBranch = () => {
+    if (this.state.branches.map((b) => b.name).includes(this.state.branchName))
+      return;
 
-    // Clears state and git-graph
-    clear = () => {
-        this.state.gitgraph.clear();
-        this.setState({
-            branches: [],
-        });
-    };
+    this.setState(() => ({
+      branches: [
+        ...this.state.branches,
+        this.state.gitgraph.branch({name:this.state.branchName,style: {
+                label: {
+                    bgColor: '#ffce52',
+                    color: 'black',
+                    strokeColor: '#ce9b00',
+                    borderRadius: 0,
+                    font: 'italic 12pt serif',
+                },
+            },}),
+      ],
+    }));
+  };
 
+  // Handles input changes
+  handleChange = (name) => (e) => {
+    this.setState({ [name]: e.currentTarget.value });
+  };
 
-    render() {
-        const branches = this.state.branches
-        return (
-            <React.Fragment>
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        this.addBranch();
-
-                    }}>
-
-                    <input type="text" onChange={this.handleChange("branchName")}/>
-                    <button>Add a branch</button>
-
-                </form>
+  // Clears state and git-graph
+  clear = () => {
+    this.state.gitgraph.clear();
+    this.setState({
+      branches: [],
+    });
+  };
 
 
-                {
-                    //To render "Commit On specific-branch" button from branches array
-                    branches.map((branch) => (
-                    <form
-                        key={branch.name}
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            this.addCommit(branch);
-                        }}
-                    >
 
-                        <input
-                            type="text"
-                            value={this.state[`commitMessage${branch.name}`]}
-                            onChange={this.handleChange(`commitMessage${branch.name}`)}
-                        />
-                        <button>Commit on {branch.name}</button>
+  render() {
+      const template={}
+    const branches = this.state.branches;
+    return (
+      <React.Fragment>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            this.addBranch();
+          }}
+        >
+          <input type="text" onChange={this.handleChange("branchName")} />
+          <button>Add a branch</button>
+        </form>
 
-                    </form>
-                ))}
-                <div>
-                {
-                    //To rende
-                    branches.map((to) =>
-                    branches.filter((from) => to.name !== from.name).map((from) => (
-                        <button
-                            key={`${to.name}->${from.name}`}
-                            onClick={() => from.merge(to)}
-                        >
-                            Merge {to.name} into {from.name}
-                        </button>
-                    )),
-                )}
-                </div>
+        {
+          //To render "Commit On specific-branch" button from branches array
+          branches.map((branch) => (
+            <form
+              key={branch.name}
+              onSubmit={(e) => {
+                e.preventDefault();
+                this.addCommit(branch);
+              }}
+            >
+              <input
+                type="text"
+                value={this.state[`commitMessage${branch.name}`]}
+                onChange={this.handleChange(`commitMessage${branch.name}`)}
+              />
+              <button>Commit on {branch.name}</button>
+            </form>
+          ))
+        }
+        <div>
+          {
+            //To render merge buttons
+            branches.map((to) =>
+              branches
+                .filter((from) => to.name !== from.name)
+                .map((from) => (
+                  <button
+                    key={`${to.name}->${from.name}`}
+                    onClick={() => from.merge(to)}
+                  >
+                    Merge {to.name} into {from.name}
+                  </button>
+                ))
+            )
+          }
+        </div>
 
-                <button onClick={this.clear}>clear</button>
+        <button onClick={this.clear}>clear</button>
+        <>
+        <Gitgraph
+          ref={this.componentRef}
+          options={{
+            orientation: "vertical",
+            author: " ",
+            template: templateExtend(TemplateName.Metro, {
+                commit: {
+                    message: {
+                        displayAuthor: false,
+                        displayBranch: true,
+                        displayHash: true,
+                    },
 
-                <Gitgraph ref={this.componentRef}
-                          children={(gitgraph) => this.setState({gitgraph})}/>
+                }
+            }),
+          }}
+          children={(gitgraph) => this.setState({ gitgraph })}
+        />
+        </>
 
-                <button onClick={() => exportComponentAsPNG(this.componentRef)}>
-                    Export As PNG
-                </button>
-            </React.Fragment>
-        );
-    }
+        <button onClick={() => exportComponentAsPNG(this.componentRef)}>
+          Export As PNG
+        </button>
+      </React.Fragment>
+    );
+  }
 }
 
-
-export default Graph
+export default Graph;
